0e8e9ca102b6792e762d24d5ba1f3ff14defec20
Lucas Brito <lucasbsilva29@gmail.com>
2024-06-10 16:05:44 -0300

    build: model building

--- tests/test_model_building.py
+++ tests/test_model_building.py
@@ -47,9 +47,58 @@ class TestModelBuilding(unittest.TestCase):
 
     def test_prepare_test_data(self):
         test_df = self.dataframe.copy()
-        test_df = model_building.prepare_test_data(test_df, test_df.columns)
+        cols = ['mana', 'attack', 'health']
+        test_df = model_building.prepare_test_data(
+            test_df,
+            cols
+        )
 
-        self.assertEqual(len(test_df.columns), len(self.dataframe.columns))
+        self.assertEqual(len(test_df.columns), len(cols))
+
+    @patch("model_building.accuracy_score")
+    @patch("model_building.ks_score")
+    @patch("model_building.f1_score")
+    @patch("model_building.precision_score")
+    def test_train_and_evaluate_models(self, mock_precision_score, mock_f1_score, mock_ks_score, mock_accuracy_score):
+        mock_accuracy_score.return_value = 1.0
+        mock_ks_score.return_value = 0.0
+        mock_f1_score.return_value = 1.0
+        mock_precision_score.return_value = 1.0
+
+        X_train, X_test, y_train, y_test = self.dataframe, self.dataframe, self.y, self.y
+
+        _, best_model_ks, best_model = model_building.train_and_evaluate_models(
+            X_train[['mana', 'health', 'attack']],
+            X_test[['mana', 'health', 'attack']],
+            y_train,
+            y_test.apply(lambda x: 1 if x == 'late' else 0)
+        )
+
+        self.assertEqual(best_model_ks, 0.0)
+        self.assertIsNotNone(best_model)
+
+    @patch("model_building.joblib.dump")
+    @patch("model_building.pd.read_csv")
+    @patch("model_building.pd.DataFrame.to_parquet")
+    @patch("model_building.pd.DataFrame.to_csv")
+    def test_save_results(self, mock_joblib_dump, mock_read_csv, mock_read_parquet, mock_to_csv):
+        mock_joblib_dump.return_value = None
+        mock_read_csv.return_value = self.dataframe
+        mock_read_csv.return_value = self.dataframe
+        mock_read_parquet.return_value = self.dataframe
+        predictions = np.random.randint(0, 1, self.dataframe.shape[0])
+        predictions = np.where(predictions > 0.5, True, False)
+
+        model_building.save_results(
+            1,
+            predictions,
+            "data"
+        )
+
+        self.assertTrue(mock_joblib_dump.called)
+        self.assertTrue(mock_read_csv.called)
+        self.assertTrue(mock_read_parquet.called)
+        self.assertTrue(mock_to_csv.called)
 
 
 if __name__ == '__main__':
